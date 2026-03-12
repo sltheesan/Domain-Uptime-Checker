f82fe95c6217a3f48baee01d80bbdb2fc515b6b0
Sasitharn Dulaan <sdulaan@gmail.com>
2026-03-12 08:49:44 +0530

    UI Issues Fixed

--- Client/src/components/layout/AppLayout.jsx
+++ Client/src/components/layout/AppLayout.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
 function AppLayout() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="app-shell">
-      <Sidebar />
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
       <div className="app-main">
-        <Topbar />
+        <Topbar onMenuClick={() => setIsSidebarOpen((prev) => !prev)} />
         <main className="app-content">
           <Outlet />
         </main>
